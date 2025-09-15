acb560fd43abd929268daac0879bbeefa1bac320
Hisham <linkedin.hisham@gmail.com>
2025-09-15 19:58:58 +0530

    Making responsive for mobile view and also adding seperate hold functionality for both players and game stop functionality

--- js/java.js
+++ js/java.js
@@ -1,206 +1,251 @@
-let running =false;
-var activeplayer=0;
-var sum=0;
-let cs=[0,0];
-let ts=[0,0];
-const limit=20;
-var flag=0;
+let running = false;
+var activeplayer = 0;
+var sum = 0;
+let cs = [0, 0]; //current score list
+let ts = [0, 0]; //total score list
+const limit = 20;
+var flag = 0;
 
 
-function GameStart(){
-    
-     var start=document.getElementById("start");
-    cs[0]=0;
-    cs[1]=0;
-    ts[0]=0;
-    ts[1]=0;
+var rollBtnOne = document.getElementById("rollOne");
+var holdBtnOne = document.getElementById("holdOne");
+var rollBtnTwo = document.getElementById("rollTwo");
+var holdBtnTwo = document.getElementById("holdTwo");
+
+var boxOne = document.getElementById("boxone");
+var boxTwo = document.getElementById("boxtwo");
+
+var start = document.getElementById("start");
+var diceLogo = document.getElementById("Face");
+// disabling all buttons initially 
+disableAllButtons();
+function disableAllButtons() {
+    rollBtnOne.disabled = true;
+    holdBtnOne.disabled = true;
+    rollBtnTwo.disabled = true;
+    holdBtnTwo.disabled = true;
+}
+
+//enabling and disabling buttons while switching players
+function updateButtons() {
+
+
+    if (activeplayer == 0) {
+        rollBtnOne.disabled = false;
+        holdBtnOne.disabled = false;
+
+        rollBtnTwo.disabled = true;
+        holdBtnTwo.disabled = true;
+    }
+    if (activeplayer == 1) {
+        rollBtnOne.disabled = true;
+        holdBtnOne.disabled = true;
+
+        rollBtnTwo.disabled = false;
+        holdBtnTwo.disabled = false;
+    }
+
+}
+
+// when start button is clicked
+function GameStart() {
+
+
+    cs[0] = 0;
+    cs[1] = 0;
+    ts[0] = 0;
+    ts[1] = 0;
     document.getElementById("cs2").innerHTML = 0;
     document.getElementById("cs1").innerHTML = 0;
     document.getElementById("ts1").innerHTML = 0;
     document.getElementById("ts2").innerHTML = 0;
 
-    if(!running){
-        flag=1;
-
-        
+    if (!running) {
+        flag = 1;
         // alert("Game has started");
         start.style.background = 'red ';
-        start.value="STOP";
-            if(activeplayer==0){
-                document.getElementById("boxone").style.boxShadow="0px 0px  30px 15px lightgreen";
-                document.getElementById("boxtwo").style.boxShadow="";
-            }
-            if(activeplayer==1){
-                document.getElementById("boxone").style.boxShadow="";
-                document.getElementById("boxtwo").style.boxShadow="0px 0px  30px 15px lightgreen";
-            }
-            
-        running=true;
+        start.value = "STOP";
+        if (activeplayer == 0) {
+            boxOne.style.boxShadow = "0px 0px  30px 15px lightgreen";
+            boxTwo.style.boxShadow = "";
+        }
+        if (activeplayer == 1) {
+            boxOne.style.boxShadow = "";
+            boxTwo.style.boxShadow = "0px 0px  30px 15px lightgreen";
+        }
+        updateButtons();
+        running = true;
 
-        
+
     }
-    else if(running){   
-        flag=0;
-        // alert("Game has stopped");
-        start.style.background = '#33B30F';
-        start.value="START"; 
-        document.getElementById("boxone").style.boxShadow="";
-        document.getElementById("boxtwo").style.boxShadow="";
-        activeplayer=0;
-        running=false;
-        
+    else if (running) {
+        stopGame();
+        diceLogo.src = "images/Red-Dice-PNG-Transparent-File.png";
+
     }
-    if(flag==0){
-        document.getElementById("Face").src="images/Red-Dice-PNG-Transparent-File.png"
-    }
+
 
 }
-function rollOne(){
-    if(flag==1 && activeplayer==0){
 
-        // var df=document.getElementById("Face");
+// function to stop game
+function stopGame() {
+    running = false;
+    flag = 0;
+    activeplayer = 0;
+
+    disableAllButtons();
+
+    boxOne.style.boxShadow = "";
+    boxTwo.style.boxShadow = "";
+    diceLogo.src = "images/Red-Dice-PNG-Transparent-File.png";
+    start.style.background = '#33B30F';
+    start.value = "START";
+
+}
+
+
+function rollOne() {
+    if (flag == 1 && activeplayer == 0) {
+
+        // var df=diceLogo;
         // df.style.color= 'white';
-        var no = Math.floor((Math.random() *6) +1);
-        
-        // document.getElementById("boxone").style.boxShadow="0px 0px  30px 15px lightgreen";
-        if(no==1){
-            document.getElementById("Face").src="images/1.png";
-            // sum=0;
-            // cs[activeplayer]=sum;
-            // document.getElementById("cs1").innerHTML = cs[1];
-            // activeplayer=1;
-            // document.getElementById("boxone").style.boxShadow="";
-            // document.getElementById("boxtwo").style.boxShadow="0px 0px  30px 15px lightgreen";
+        var no = Math.floor((Math.random() * 6) + 1);
+
+        // boxOne.style.boxShadow="0px 0px  30px 15px lightgreen";
+        if (no == 1) {
+            diceLogo.src = "images/1.png";
+
         }
-        if(no==2){
-            document.getElementById("Face").src="images/2.png";
+        if (no == 2) {
+            diceLogo.src = "images/2.png";
         }
-        if(no==3){
-            document.getElementById("Face").src="images/3.png";
+        if (no == 3) {
+            diceLogo.src = "images/3.png";
         }
-        if(no==4){
-            document.getElementById("Face").src="images/4.png";
+        if (no == 4) {
+            diceLogo.src = "images/4.png";
         }
-        if(no==5){
-            document.getElementById("Face").src="images/5.png";
+        if (no == 5) {
+            diceLogo.src = "images/5.png";
         }
-        if(no==6){
-            document.getElementById("Face").src="images/6.png";
+        if (no == 6) {
+            diceLogo.src = "images/6.png";
         }
-    
-        if(no!=1 ){
-            
-            cs[0]=cs[0]+no;
+        //adding values when not equal to 1
+        if (no != 1) {
+
+            cs[0] = cs[0] + no;
             document.getElementById("cs1").innerHTML = cs[0];
-            
-        } 
-        if(no==1){
-            // sum=0;
-            cs[0]=0;
 
-            // alert("nextplayer")
-            document.getElementById("cs1").innerHTML = cs[0];
-            document.getElementById("boxone").style.boxShadow="";
-            document.getElementById("boxtwo").style.boxShadow="0px 0px  30px 15px lightgreen";
-            activeplayer=1;
-        } 
-        
-        // if(no!=1 && activeplayer==1){
-        //     cs[activeplayer]=cs[activeplayer]+no;
-        //     document.getElementById("cs2").innerHTML = cs;
-        // } 
-        
+        }
+        //set current score to zero when we get one on dice
+        /*   if (no == 1) {
+              // sum=0;
+              cs[0] = 0;
+  
+              // alert("nextplayer")
+              document.getElementById("cs1").innerHTML = cs[0];
+              boxOne.style.boxShadow = "";
+              boxTwo.style.boxShadow = "0px 0px  30px 15px lightgreen";
+              activeplayer = 1;
+              updateButtons();
+          } */
+
+
+
 
     }
-    
+
 }
 
-function rollTwo(){
-    if(flag==1 && activeplayer==1){
+function rollTwo() {
+    if (flag == 1 && activeplayer == 1) {
 
-        var no=Math.floor((Math.random() *6) +1);
-        
-        
-        if(no==1){
-            document.getElementById("Face").src="images/1.png";
-          
+        var no = Math.floor((Math.random() * 6) + 1);
+
+
+        if (no == 1) {
+            diceLogo.src = "images/1.png";
+
         }
-        if(no==2){
-            document.getElementById("Face").src="images/2.png";
+        if (no == 2) {
+            diceLogo.src = "images/2.png";
         }
-        if(no==3){
-            document.getElementById("Face").src="images/3.png";
+        if (no == 3) {
+            diceLogo.src = "images/3.png";
         }
-        if(no==4){
-            document.getElementById("Face").src="images/4.png";
+        if (no == 4) {
+            diceLogo.src = "images/4.png";
         }
-        if(no==5){
-            document.getElementById("Face").src="images/5.png";
+        if (no == 5) {
+            diceLogo.src = "images/5.png";
         }
-        if(no==6){
-            document.getElementById("Face").src="images/6.png";
+        if (no == 6) {
+            diceLogo.src = "images/6.png";
         }
-    
-        if(no!=1 ){
-            
-            cs[1]=cs[1]+no;
+
+        if (no != 1) {
+
+            cs[1] = cs[1] + no;
             document.getElementById("cs2").innerHTML = cs[1];
-        } 
-        if(no==1 ){
-            
-            cs[1]=0;
-            // alert("nextplayer")
-           
-            document.getElementById("cs2").innerHTML = cs[1];
-            document.getElementById("boxone").style.boxShadow="0px 0px  30px 15px lightgreen";
-            document.getElementById("boxtwo").style.boxShadow="";
-            activeplayer=0;
-        } 
-        
-      
-        
-
-    }
-    
-}
+        }
+        /*   if (no == 1) {
+  
+              cs[1] = 0;
+              // alert("nextplayer")
+  
+              document.getElementById("cs2").innerHTML = cs[1];
+              boxOne.style.boxShadow = "0px 0px  30px 15px lightgreen";
+              boxTwo.style.boxShadow = "";
+              activeplayer = 0;
+              updateButtons();
+          } */
 
 
-function hold(){
-    
-    
-        
-    if(flag==1 && activeplayer==0){
-            
-            ts[0]=ts[0]+cs[0];
-            cs[0]=0;
-            document.getElementById("ts1").innerHTML =ts[0];
-            document.getElementById("cs1").innerHTML = 0;
-            document.getElementById("boxone").style.boxShadow="";
-            document.getElementById("boxtwo").style.boxShadow="0px 0px  30px 15px lightgreen";
-            activeplayer=1;
-            if(ts[0]>=100){
-                alert("Player 1 wins")
-            }
 
-    }
 
-    
-    else if(flag==1 && activeplayer==1){
-            
-            ts[1]=ts[1]+cs[1];
-            cs[1]=0; 
-            document.getElementById("ts2").innerHTML =ts[1];
-            document.getElementById("cs2").innerHTML = 0;
-            document.getElementById("boxone").style.boxShadow="0px 0px  30px 15px lightgreen";
-            document.getElementById("boxtwo").style.boxShadow="";
-            activeplayer=0;
-            
-            if(ts[1]>=100){
-                alert("Player 2 wins")
-            }
     }
 
 }
 
-    
+
+function hold() {
+    if (flag == 1 && activeplayer == 0) {
+
+        ts[0] = ts[0] + cs[0];
+        cs[0] = 0;
+        document.getElementById("ts1").innerHTML = ts[0];
+        document.getElementById("cs1").innerHTML = 0;
+        boxOne.style.boxShadow = "";
+        boxTwo.style.boxShadow = "0px 0px  30px 15px lightgreen";
+        activeplayer = 1;
+        updateButtons();
+        if (ts[0] >= 100) {
+            alert("Player 1 wins");
+            stopGame();
+
+        }
+
+    }
+
+
+    else if (flag == 1 && activeplayer == 1) {
+
+        ts[1] = ts[1] + cs[1];
+        cs[1] = 0;
+        document.getElementById("ts2").innerHTML = ts[1];
+        document.getElementById("cs2").innerHTML = 0;
+        boxOne.style.boxShadow = "0px 0px  30px 15px lightgreen";
+        boxTwo.style.boxShadow = "";
+        activeplayer = 0;
+        updateButtons();
+        if (ts[1] >= 100) {
+            alert("Player 2 wins");
+            stopGame();
+        }
+    }
+
+}
+
+
 
